d52bd7ce5687bc8742ec7ffb071f06111b766465
Sergey Nevmerzhitsky <nevmerzhitsky@whitecode.ru>
2015-11-25 17:21:02 +0300

    Changed init.php content for better autoloading by Composer.

--- src/init.php
+++ src/init.php
@@ -1,15 +1,15 @@
 <?php
 
-class ApplicationException extends Exception {}
-
-class DatabaseException extends ApplicationException {}
-
 if (!defined('APPBASE')) {
     define('APPBASE', __DIR__ . '/../../../');
 }
 if (!defined('WL_INCBASE')) {
     define('WL_INCBASE', __DIR__ . '/');
 }
+
+require_once WL_INCBASE . 'exceptions.php';
+require_once WL_INCBASE . 'functions.php';
+require_once WL_INCBASE . 'profiling.php';
 
 require_once WL_INCBASE . 'AbstractHttpController.php';
 require_once WL_INCBASE . 'BytesMaster.php';
@@ -19,6 +19,3 @@ require_once WL_INCBASE . 'Pagination.php';
 require_once WL_INCBASE . 'DB/QueryWhere.php';
 require_once WL_INCBASE . 'DB/QueryOrder.php';
 require_once WL_INCBASE . 'Service/JsonService.php';
-
-require_once WL_INCBASE . 'functions.php';
-require_once WL_INCBASE . 'profiling.php';
