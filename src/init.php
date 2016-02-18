e0f636eadf5ee8facc87f0e8792d8e5b02bd2bfd
Sergey Nevmerzhitsky <nevmerzhitsky@whitecode.ru>
2016-02-18 18:45:38 +0300

    Added ElasticSearchHugeSerializer.

--- src/init.php
+++ src/init.php
@@ -10,10 +10,11 @@ require_once WL_INCBASE . 'exceptions.php';
 require_once WL_INCBASE . 'functions.php';
 require_once WL_INCBASE . 'profiling.php';
 
-require_once WL_INCBASE . 'HttpControllerException.php';
 require_once WL_INCBASE . 'AbstractHttpController.php';
 require_once WL_INCBASE . 'BytesMaster.php';
 require_once WL_INCBASE . 'Config.php';
+require_once WL_INCBASE . 'ElasticSearchHugeSerializer.php';
+require_once WL_INCBASE . 'HttpControllerException.php';
 require_once WL_INCBASE . 'Navbar.php';
 require_once WL_INCBASE . 'Pagination.php';
 require_once WL_INCBASE . 'DB/QueryWhere.php';
